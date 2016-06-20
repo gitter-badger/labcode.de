a4b15f5ccc33ef4555da904610e567e5cba18074
Leo Maroni <labcode-de@users.noreply.github.com>
2016-06-20 20:29:38 +0200

    Merge gh-page to master (#1)
    * Github Commit Add
    
    * Rename _icons to icons
    
    * Add Silktide Cookie-Consent
    
    * Add Cookie Message on Webapps Pages
    
    * https to http and CookieConsent Updated
    
    * Add Required to Contact Form
    
    * Move Contact.php to LabCode Data Server

--- js/github.js
+++ js/github.js
@@ -1,16 +1,15 @@
-// Github Latest Commit
-if ($('.github-commit').length) { // Checks if widget div exists (Index only)
-  $.ajax({
-    url: "https://api.github.com/repos/labcode-de/labcode.de/commits/gh-pages",
-    dataType: "json",
-    success: function (data) {
-      var sha = data.sha,
-          date = jQuery.timeago(data.commit.author.date);
-      if (window_width < 1120) {
-        sha = sha.substring(0,7);
-      }
-      $('.github-commit').find('.date').html(date);
-      $('.github-commit').find('.sha').html(sha).attr('href', data.html_url);
+(function($){
+  $(function(){
+    // Github Latest Commit
+    if ($('.github-commit').length) { // Checks if widget div exists (Index only)
+      $.ajax({
+        url: "https://api.github.com/repos/dogfalo/materialize/commits/master",
+        dataType: "json",
+        success: function (data) {
+          var sha = data.sha
+          $('.github-commit').find('.sha').html(sha).attr('href', data.html_url);
+        }
+      });
     }
   });
-}
+})(jQuery);
